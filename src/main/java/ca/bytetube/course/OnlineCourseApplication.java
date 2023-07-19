d21966b2a6a31422d769e9e78c21040a5bd96c75
isisisisisisithch <dallucus@gmail.com>
2023-07-19 14:42:09 -0400

    1.初始化项目（修改springboot版本，尽量和我使用相同的版本）

---
+++ src/main/java/ca/bytetube/course/OnlineCourseApplication.java
@@ -0,0 +1,13 @@
+package ca.bytetube.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/bytetube/course/OnlineCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bytetube.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
