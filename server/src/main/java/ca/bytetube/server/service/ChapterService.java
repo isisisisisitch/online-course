97a9769f74c4891c076c0bca925d68c47df5417a
isisisisisisithch <dallucus@gmail.com>
2023-07-26 16:26:33 -0400

    45.集成分页插件pagehelper，注意页码从1开始

--- server/src/main/java/ca/bytetube/server/service/ChapterService.java
+++ server/src/main/java/ca/bytetube/server/service/ChapterService.java
@@ -4,6 +4,7 @@ import ca.bytetube.server.domain.Chapter;
 import ca.bytetube.server.domain.ChapterExample;
 import ca.bytetube.server.dto.ChapterDto;
 import ca.bytetube.server.mapper.ChapterMapper;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,7 @@ public class ChapterService {
     private ChapterMapper chapterMapper;
 
     public List<ChapterDto> list() {
+        PageHelper.startPage(1, 1);
         ChapterExample chapterExample = new ChapterExample();
         List<Chapter> chapterList = chapterMapper.selectByExample(chapterExample);
         List<ChapterDto> chapterDtoList = new ArrayList<>();
