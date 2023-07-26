411b3ae89ee7650646f7d41f3e42018f547669b2
isisisisisisithch <dallucus@gmail.com>
2023-07-26 13:23:07 -0400

    43.解决gateway跨域问题

--- gateway/src/main/java/ca/bytetube/gateway/GatewayApplication.java
+++ gateway/src/main/java/ca/bytetube/gateway/GatewayApplication.java
@@ -5,7 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -17,8 +22,28 @@ public class GatewayApplication {
         SpringApplication app = new SpringApplication(GatewayApplication.class);
         Environment env = app.run(args).getEnvironment();
         LOG.info("start successfully！！");
-        LOG.info("System add: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+        LOG.info("Gateway add: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
+
+    /**
+     * 配置跨域
+     */
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(Boolean.TRUE);
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+
 
 }
 
