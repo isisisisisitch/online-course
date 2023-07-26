059217edd4f05a7998ad5573d006ae37581ed656
isisisisisisithch <dallucus@gmail.com>
2023-07-26 11:50:04 -0400

    34.增加大章列表查询接口

---
+++ business/src/main/java/ca/bytetube/business/controller/ChapterController.java
@@ -0,0 +1,21 @@
+package ca.bytetube.business.controller;
+
+import ca.bytetube.server.domain.Chapter;
+import ca.bytetube.server.service.ChapterService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+public class ChapterController {
+
+    @Resource
+    private ChapterService chapterService;
+
+    @RequestMapping("/chapter")
+    public List<Chapter> chapter() {
+        return chapterService.list();
+    }
+}
---
+++ server/src/main/java/ca/bytetube/server/service/ChapterService.java
@@ -0,0 +1,23 @@
+package ca.bytetube.server.service;
+
+import ca.bytetube.server.domain.Chapter;
+import ca.bytetube.server.domain.ChapterExample;
+import ca.bytetube.server.mapper.ChapterMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class ChapterService {
+
+    @Resource
+    private ChapterMapper chapterMapper;
+
+    public List<Chapter> list() {
+        ChapterExample chapterExample = new ChapterExample();
+        chapterExample.createCriteria().andIdEqualTo("1");
+        chapterExample.setOrderByClause("id desc");
+        return chapterMapper.selectByExample(chapterExample);
+    }
+}
