17b4a30b3e304d163fe53d081c53aa3c36443323
isisisisisisithch <dallucus@gmail.com>
2023-07-26 17:28:29 -0400

    51.增加复制工具类CopyUtil，封装BeanUtils.copyProperties，简化单实体复制和列表复制的代码

--- server/src/main/java/ca/bytetube/server/service/ChapterService.java
+++ server/src/main/java/ca/bytetube/server/service/ChapterService.java
@@ -5,6 +5,7 @@ import ca.bytetube.server.domain.ChapterExample;
 import ca.bytetube.server.dto.ChapterDto;
 import ca.bytetube.server.dto.PageDto;
 import ca.bytetube.server.mapper.ChapterMapper;
+import ca.bytetube.server.util.CopyUtil;
 import ca.bytetube.server.util.UuidUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -27,20 +28,22 @@ public class ChapterService {
         List<Chapter> chapterList = chapterMapper.selectByExample(chapterExample);
         PageInfo<Chapter> pageInfo = new PageInfo<>(chapterList);
         pageDto.setTotal(pageInfo.getTotal());
-        List<ChapterDto> chapterDtoList = new ArrayList<>();
-        for (int i = 0, l = chapterList.size(); i < l; i++) {
-            Chapter chapter = chapterList.get(i);
-            ChapterDto chapterDto = new ChapterDto();
-            BeanUtils.copyProperties(chapter, chapterDto);
-            chapterDtoList.add(chapterDto);
-        }
+//        List<ChapterDto> chapterDtoList = new ArrayList<ChapterDto>();
+//        for (int i = 0, l = chapterList.size(); i < l; i++) {
+//            Chapter chapter = chapterList.get(i);
+//            ChapterDto chapterDto = new ChapterDto();
+//            BeanUtils.copyProperties(chapter, chapterDto);
+//            chapterDtoList.add(chapterDto);
+//        }
+        List<ChapterDto> chapterDtoList = CopyUtil.copyList(chapterList, ChapterDto.class);
         pageDto.setList(chapterDtoList);
     }
 
     public void save(ChapterDto chapterDto) {
         chapterDto.setId(UuidUtil.getShortUuid());
-        Chapter chapter = new Chapter();
-        BeanUtils.copyProperties(chapterDto, chapter);
+//        Chapter chapter = new Chapter();
+//        BeanUtils.copyProperties(chapterDto, chapter);
+        Chapter chapter = CopyUtil.copy(chapterDto, Chapter.class);
         chapterMapper.insert(chapter);
     }
 }
