189905c8e17a877e169b1bdef88409e6a56f3d4b
isisisisisisithch <dallucus@gmail.com>
2023-07-31 17:37:39 -0400

    72.     1. 生成器开发：增加生成vue界面代码。     2. 小节管理增删改查功能测试成功

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -16,21 +16,19 @@ values ('00000001', '00000000', 'testChapter2');
 
 -- 小节
 DROP TABLE IF EXISTS `section`;
-CREATE TABLE `section`
-(
-    `id`         CHAR(8)     NOT NULL DEFAULT '' COMMENT 'ID',
-    `title`      VARCHAR(50) NOT NULL COMMENT 'title',
-    `course_id`  CHAR(8) COMMENT 'course|course.id',
-    `chapter_id` CHAR(8) COMMENT 'chapter|chapter.id',
-    `video`      VARCHAR(200) COMMENT 'video',
-    `time`       INT COMMENT 'time|s',
-    `charge`     CHAR(1) COMMENT 'charge|C charge；F free',
-    `sort`       INT COMMENT 'sort',
-    `created_at` DATETIME(3) COMMENT 'created time',
-    `updated_at` DATETIME(3) COMMENT 'updated time',
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4 COMMENT ='section';
+CREATE TABLE `section` (
+  `id` CHAR(8) NOT NULL DEFAULT '' COMMENT 'ID',
+  `title` VARCHAR(50) NOT NULL COMMENT '标题',
+  `course_id` CHAR(8) COMMENT '课程|course.id',
+  `chapter_id` CHAR(8) COMMENT '大章|chapter.id',
+  `video` VARCHAR(200) COMMENT '视频',
+  `time` INT COMMENT '时长|单位秒',
+  `charge` CHAR(1) COMMENT '收费|C 收费；F 免费',
+  `sort` INT COMMENT '顺序',
+  `created_at` DATETIME(3) COMMENT '创建时间',
+  `updated_at` DATETIME(3) COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='小节';
 
 INSERT INTO `section` (id, title, course_id, chapter_id, video, time, charge, sort, created_at, updated_at)
 VALUES ('00000001', 'testSection1', '00000001', '00000000', '', 500, 'F', 1, now(), now());
