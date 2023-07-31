7861022623e65e9a54c59e6d488eedafdf6ce190
isisisisisisithch <dallucus@gmail.com>
2023-07-31 16:36:42 -0400

    66.生成器开发：增加生成service层的代码(dto的生成后面会解决)

--- generator/src/main/java/ca/bytetube/generator/server/ServerGenerator.java
+++ generator/src/main/java/ca/bytetube/generator/server/ServerGenerator.java
@@ -1,15 +1,24 @@
 package ca.bytetube.generator.server;
 
+
 import ca.bytetube.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ServerGenerator {
-    static String toPath = "generator/src/main/java/ca/bytetube/generator/test/";
+    static String toServicePath = "server/src/main/java/ca/bytetube/server/service/";
 
     public static void main(String[] args) throws IOException, TemplateException {
-        FreemarkerUtil.initConfig("test.ftl");
-        FreemarkerUtil.generator(toPath + "Test.java");
+        String Domain = "Section";
+        String domain = "section";
+        Map<String, Object> map = new HashMap<>();
+        map.put("Domain", Domain);
+        map.put("domain", domain);
+
+        FreemarkerUtil.initConfig("service.ftl");
+        FreemarkerUtil.generator(toServicePath + Domain + "Service.java", map);
     }
 }
