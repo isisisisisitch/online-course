23bfffaf14202a7ac1fbac572129eabf2ab41933
isisisisisisithch <dallucus@gmail.com>
2023-08-09 16:23:24 -0400

    109.文件上传功能开发：增加springboot静态资源配置，配置文件访问路径

---
+++ file/src/main/java/ca/bytetube/file/config/SpringMvcConfig.java
@@ -0,0 +1,15 @@
+package ca.bytetube.file.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/f/**").addResourceLocations("file:/Users/dalluo/Desktop/online_course/");
+    }
+    //http://127.0.0.1:9003/file/f/teacher/35zrWWBV-logo.jpg
+}
